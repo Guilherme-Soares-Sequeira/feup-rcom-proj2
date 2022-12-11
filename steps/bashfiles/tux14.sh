0eda2199ad7ac47d26ce78e6c44b904535ffc2eb
Guilherme-Soares-Sequeira <72896498+Guilherme-Soares-Sequeira@users.noreply.github.com>
2022-12-11 20:08:59 +0000

    Update tux14.sh

--- steps/bashfiles/tux14.sh
+++ steps/bashfiles/tux14.sh
@@ -6,11 +6,9 @@ systemctl restart networking
 # config tux4 ip
 ifconfig eth0 up
 ifconfig eth0 172.16.10.254/24
-ifconfig eth0
 
 ifconfig eth1 up
 ifconfig eth1 172.16.11.253/24
-ifconfig eth1
 
 # enable IP forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -18,9 +16,10 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # disable ICMP echo-ignore-broadcast
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 
-read -n 1 -p "Set up the bridges and then click any key to continue."
+read -n 1 -p "Set up the router ip and then click any key to continue."
 
-# add Rc as a router
-route add -net 172.16.1.0/24 gw 172.16.11.254
-
+# add Rc as the default router
 route add default gw 172.16.11.254
+
+# configure DNS
+echo 'nameserver 172.16.1.1' > /etc/resolv.conf
