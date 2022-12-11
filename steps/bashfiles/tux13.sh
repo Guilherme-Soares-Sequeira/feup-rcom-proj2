bfa1607ca05bf8ca27eaeea774623771296be1c7
Guilherme-Soares-Sequeira <72896498+Guilherme-Soares-Sequeira@users.noreply.github.com>
2022-12-11 19:56:35 +0000

    Update tux13.sh

--- steps/bashfiles/tux13.sh
+++ steps/bashfiles/tux13.sh
@@ -8,9 +8,11 @@ ifconfig eth0 up
 ifconfig eth0 172.16.10.1/24
 ifconfig eth0
 
+read -n 1 -p "Set up the ip address in tuxY4 and tuxY2 then press any key to continue."
+
 # add route to tux2 through tux4
 route add -net 172.16.11.0/24 gw 172.16.10.254
 
-read -n 1 -p "Set up the bridges and then click any key to continue."
+read -n 1 -p "Set up the router ip and then click any key to continue."
 
 route add default gw 172.16.10.254
