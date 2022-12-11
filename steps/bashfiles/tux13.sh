1fe4f0dfc6f5e90cf74a75cff690f672ca5c1400
Guilherme-Soares-Sequeira <72896498+Guilherme-Soares-Sequeira@users.noreply.github.com>
2022-12-11 20:10:00 +0000

    Update tux13.sh

--- steps/bashfiles/tux13.sh
+++ steps/bashfiles/tux13.sh
@@ -15,4 +15,8 @@ route add -net 172.16.11.0/24 gw 172.16.10.254
 
 read -n 1 -p "Set up the router ip and then click any key to continue."
 
+# configure tuxY4 to be the default router
 route add default gw 172.16.10.254
+
+# configure DNS
+echo 'nameserver 172.16.1.1' > /etc/resolv.conf
