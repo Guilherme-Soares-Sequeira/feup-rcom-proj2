bcd04370c44c42a1d5e0f7280ff2d3d874089c34
Guilherme-Soares-Sequeira <72896498+Guilherme-Soares-Sequeira@users.noreply.github.com>
2022-12-11 19:55:42 +0000

    Update tux12.sh

--- steps/bashfiles/tux12.sh
+++ steps/bashfiles/tux12.sh
@@ -8,11 +8,14 @@ ifconfig eth0 up
 ifconfig eth0 172.16.11.1/24
 ifconfig eth0
 
+read -n 1 -p "Set up the ip address in tuxY4 and tuxY3 then press any key to continue."
+
 # add route to tux3 through tux4
 route add -net 172.16.10.0/24 gw 172.16.11.253
 
-read -n 1 -p "Set up the bridges and then click any key to continue."
+read -n 1 -p "Set up the ip address in the router then press any key to continue."
 
+# make the router the default gateway
 route add default gw 172.16.11.254
 
 #maybe this is not supposed to stay here
